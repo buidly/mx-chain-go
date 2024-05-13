a20387a77e04060d9604d6a1db8f257af5084868
Rares <6453351+raress96@users.noreply.github.com>
2024-05-13 16:45:35 +0300

    Update configs and contracts.

--- vm/systemSmartContracts/delegationMidas.go
+++ vm/systemSmartContracts/delegationMidas.go
@@ -163,7 +163,7 @@ func (d *delegationMidas) Execute(args *vmcommon.ContractCallInput) vmcommon.Ret
 	case "modifyTotalDelegationCap":
 		return d.modifyTotalDelegationCap(args)
 	case "updateRewards":
-		return d.updateRewards(args) // TODO: This should be handled by Abstract Staking contract; Elite Account status of the Staking Agency operator should somehow also be taken into consideration to reduce Staking Agency Fees?
+		return d.updateRewards(args)
 	case claimRewards:
 		return d.claimRewards(args)
 	case "getRewardData":
